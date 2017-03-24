b5c2b128de26961832f549cb9fa222050df58ffd
lvliuzhong <lvliuzhong@wondertek.com.cn>
2017-03-24 13:52:00 +0800

    去除多余的参数

--- src/main/java/com/wondertek/mobilevideo/core/recommend/util/RequestConstants.java
+++ src/main/java/com/wondertek/mobilevideo/core/recommend/util/RequestConstants.java
@@ -5,130 +5,218 @@ public class RequestConstants {
 	 * 用户默认标签
 	 */
 	public static final String P_DEFAULT_USERTAG = "user.tag.default";
+	/**
+	 * 用户默认标签
+	 */
 	public static String V_DEFAULT_USERTAG;
 	/**
 	 * 用户标签默认分数
 	 */
 	public static final String P_DEFAULT_USERTAG_SCORE = "user.tag.score.default";
+	/**
+	 * 用户标签默认分数
+	 */
 	public static Double V_DEFAULT_USERTAG_SCORE = 10d;
 	/**
-	 * 请求默认每页条数
+	 * 请求时，默认每页条数
 	 */
 	public static final String P_DEFAUL_REQUEST_LIMIT = "request.default.limit";
-	public static int V_DEFAUL_REQUEST_LIMIT = 10;
+	/**
+	 * 请求时，默认每页条数
+	 */
+	public static int V_DEFAUL_REQUEST_LIMIT = 100;
 	/**
 	 * 请求用户标签时一级分类的最大个数
 	 */
 	public static final String P_DEFAUL_REQUEST_TAG_CAT_MAX = "request.default.tag.cat.max";
+	/**
+	 * 请求用户标签时一级分类的最大个数
+	 */
 	public static int V_DEFAUL_REQUEST_TAG_CAT_MAX = 10;
 	/**
 	 * 请求用户标签时一级分类下的普通标签最大个数
 	 */
 	public static final String P_DEFAUL_REQUEST_TAG_CATITEM_MAX = "request.default.tag.catitem.max";
+	/**
+	 * 请求用户标签时一级分类下的普通标签最大个数
+	 */
 	public static int V_DEFAUL_REQUEST_TAG_CATITEM_MAX = 10;
 	/**
 	 * 请求用户标签时一级分类下的推荐标签最大个数
 	 */
 	public static final String P_DEFAUL_REQUEST_TAG_RECOMD_MAX = "request.default.tag.recomd.max";
+	/**
+	 * 请求用户标签时一级分类下的推荐标签最大个数
+	 */
 	public static int V_DEFAUL_REQUEST_TAG_RECOMD_MAX = 10;
 	/**
 	 * 正常调用请求最多查询多少条记录进行分页
 	 * @deprecated
 	 */
 	public static final String P_DEFAULT_SEARCH_COUNT_MAX = "search.default.count.max";
+	/**
+	 * 正常调用请求最多查询多少条记录进行分页
+	 * @deprecated
+	 */
 	public static int V_DEFAULT_SEARCH_COUNT_MAX = 100;
 	/**
 	 * 打印返回大数据
 	 */
 	public static final String P_PRINT_REQUEST_ENABLE = "print.request.enable";
+	/**
+	 * 打印返回大数据
+	 */
 	public static Boolean V_PRINT_REQUEST_ENABLE = Boolean.TRUE;
 	/**
 	 * 搜索请求地址
 	 */
 	public static final String P_SEARCH_URL = "search.url";
+	/**
+	 * 搜索请求地址
+	 */
 	public static String V_SEARCH_URL;
 	/**
 	 * 搜索时，带推荐标签
+	 * @deprecated
 	 */
 	public static final String P_SEARCH_RECOMD_ENABLE = "search.recomd.enable";
+	/**
+	 * 搜索时，带推荐标签
+	 * @deprecated
+	 */
 	public static Boolean V_SEARCH_RECOMD_ENABLE = Boolean.TRUE;
 	
 	/**
-	 * 调用搜索引擎时，查询一次一级分类最多查询多少条记录
+	 * 调用搜索引擎时，是否调用搜索引擎
 	 */
 	public static final String P_DEFAULT_SEARCH_ENABLE = "search.default.enable";
+	/**
+	 * 调用搜索引擎时，是否调用搜索引擎
+	 */
 	public static Boolean V_DEFAULT_SEARCH_ENABLE = Boolean.TRUE;
 	/**
 	 * 调用搜索引擎时，查询一次一级分类最多查询多少条记录
 	 */
 	public static final String P_DEFAULT_SEARCH_LIMIT = "search.default.limit";
+	/**
+	 * 调用搜索引擎时，查询一次一级分类最多查询多少条记录
+	 */
 	public static int V_DEFAULT_SEARCH_LIMIT = 20;
 	/**
 	 * 调用搜索引擎时，查询一次一级分类下的固定标签最多查询多少条记录
 	 * @deprecated
 	 */
 	public static final String P_DEFAULT_SEARCH_LIMIT_CATITEM = "search.default.limit.catItem";
+	/**
+	 * 调用搜索引擎时，查询一次一级分类下的固定标签最多查询多少条记录
+	 * @deprecated
+	 */
 	public static int V_DEFAULT_SEARCH_LIMIT_CATITEM = 5;
 	/**
 	 * 调用搜索引擎时，最多查询多少个一级分类
 	 */
 	public static final String P_DEFAULT_SEARCH_CAT_MAX = "search.default.cat.max";
+	/**
+	 * 调用搜索引擎时，最多查询多少个一级分类
+	 */
 	public static int V_DEFAULT_SEARCH_CAT_MAX = 3;
 	/**
 	 * 调用搜索引擎时，查询一次一级分类最多分多少次查询
 	 * @deprecated
 	 */
 	public static final String P_DEFAULT_SEARCH_CAT_PER_MAX = "search.default.cat.per.max";
+	/**
+	 * 调用搜索引擎时，查询一次一级分类最多分多少次查询
+	 * @deprecated
+	 */
 	public static int V_DEFAULT_SEARCH_CAT_PER_MAX = 2;
 	/**
 	 * 调用搜索引擎时，最多查询多少个一级分类下的标签
 	 */
 	public static final String P_DEFAULT_SEARCH_CATITEM_MAX = "search.default.catitem.max";
+	/**
+	 * 调用搜索引擎时，最多查询多少个一级分类下的标签
+	 */
 	public static int V_DEFAULT_SEARCH_CATITEM_MAX = 5;
+	/**
+	 * 调用搜索引擎时，是否带权重分数查询
+	 */
+	public static final String P_DEFAULT_SEARCH_WITH_WEIGHT = "search.default.with.weight";
+	/**
+	 * 调用搜索引擎时，是否带权重分数查询
+	 */
+	public static Boolean V_DEFAULT_SEARCH_WITH_WEIGHT = Boolean.FALSE;
 	/**
 	 * 人工推荐时，最多查询多少个一级分类
 	 */
 	public static final String P_DEFAULT_RECOMD_CAT_MAX = "recomd.default.cat.max";
+	/**
+	 * 人工推荐时，最多查询多少个一级分类
+	 */
 	public static int V_DEFAULT_RECOMD_CAT_MAX = 5;
 	/**
 	 * 人工推荐时，最多查询多少个一级分类下推荐标签
 	 */
 	public static final String P_DEFAULT_RECOMD_CATITEM_MAX = "recomd.default.catitem.max";
+	/**
+	 * 人工推荐时，最多查询多少个一级分类下推荐标签
+	 */
 	public static int V_DEFAULT_RECOMD_CATITEM_MAX = 10;
 	/**
 	 * 综合推荐时，专题推荐比例，只可为正小数，否则为0，如0.02
 	 */
 	public static final String P_DEFAULT_RECOMD_SPECIALTOPIC_RATIO = "recomd.default.specialTopic.ratio";
+	/**
+	 * 综合推荐时，专题推荐比例，只可为正小数，否则为0，如0.02
+	 */
 	public static Double V_DEFAULT_RECOMD_SPECIALTOPIC_RATIO = 0d;
 	/**
 	 * 综合推荐时，组合内容推荐比例，只可为正小数，否则为0，如0.02
 	 */
 	public static final String P_DEFAULT_RECOMD_COMBINEDCONT_RATIO = "recomd.default.combinedCont.ratio";
+	/**
+	 * 综合推荐时，组合内容推荐比例，只可为正小数，否则为0，如0.02
+	 */
 	public static Double V_DEFAULT_RECOMD_COMBINEDCONT_RATIO = 0d;
 	/**
 	 * 综合推荐时，大图内容推荐比例，只可为正小数，否则为0，如0.02
 	 */
 	public static final String P_DEFAULT_RECOMD_BIGPICCONT_RATIO = "recomd.default.bigPicCont.ratio";
+	/**
+	 * 综合推荐时，大图内容推荐比例，只可为正小数，否则为0，如0.02
+	 */
 	public static Double V_DEFAULT_RECOMD_BIGPICCONT_RATIO = 0d;
 	/**
 	 * 综合推荐时，多图内容推荐比例，只可为正小数，否则为0，如0.02
 	 */
 	public static final String P_DEFAULT_RECOMD_MULTIPICCONT_RATIO = "recomd.default.multiPicCont.ratio";
+	/**
+	 * 综合推荐时，多图内容推荐比例，只可为正小数，否则为0，如0.02
+	 */
 	public static Double V_DEFAULT_RECOMD_MULTIPICCONT_RATIO = 0d;
 	/**
 	 * 推荐时，一级标签默认权重
 	 */
 	public static final String P_DEFAULT_RECOMD_TAG_CAT_WEIGHT = "recomd.default.tag.cat.weight";
+	/**
+	 * 推荐时，一级标签默认权重
+	 */
 	public static Double V_DEFAULT_RECOMD_TAG_CAT_WEIGHT = 100d;
 	/**
 	 * 推荐时，普通标签默认权重
 	 */
 	public static final String P_DEFAULT_RECOMD_TAG_ITEM_WEIGHT = "recomd.default.tag.item.weight";
+	/**
+	 * 推荐时，普通标签默认权重
+	 */
 	public static Double V_DEFAULT_RECOMD_TAG_ITEM_WEIGHT = 100d;
 	/**
 	 * 推荐时，推荐标签默认权重
 	 */
 	public static final String P_DEFAULT_RECOMD_TAG_RCMDITEM_WEIGHT = "recomd.default.tag.rcmditem.weight";
+	/**
+	 * 推荐时，推荐标签默认权重
+	 */
 	public static Double V_DEFAULT_RECOMD_TAG_RCMDITEM_WEIGHT = 100d;
 	
 	
